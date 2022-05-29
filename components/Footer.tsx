3858fe24f4c3ba62d208e97c71d102c1458b720a
Majorfi <major-tom.eth@pm.me>
2022-05-29 14:01:04 +0200

    fix: add link decription for accessibility

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import	React, {ReactElement}		from	'react';
 import	IconTwitter					from	'components/icons/IconTwitter';
 import	IconMedium					from	'components/icons/IconMedium';
-// import	IconDiscord					from	'components/icons/IconDiscord';
 import	IconDiscourse				from	'components/icons/IconDiscourse';
 
 function	Footer(): ReactElement {
@@ -13,8 +12,6 @@ function	Footer(): ReactElement {
 						{'This project is in beta. Use at your own risk.'}
 					</b>
 					<div className={'flex flex-row items-center'}>
-						{/* <div className={'mr-2 w-2 h-2 bg-black rounded-full'} />
-						<p className={'text-xs'}>{'Disconnected'}</p> */}
 						<div className={'mr-2 w-2 h-2 bg-transparent rounded-full border border-black'} />
 						<p className={'text-xs'}>{'Connected'}</p>
 					</div>
@@ -22,21 +19,19 @@ function	Footer(): ReactElement {
 
 				<div className={'px-2 ml-auto text-black hover:text-black-2 transition-colors cursor-pointer'}>
 					<a href={'https://twitter.com/thekeep3r'} target={'_blank'} rel={'noreferrer'}>
+						<span className={'sr-only'}>{'Check Twitter account'}</span>
 						<IconTwitter className={'w-8 h-8'} />
 					</a>
 				</div>
 				<div className={'px-2 text-black hover:text-black-2 transition-colors cursor-pointer'}>
 					<a href={'https://medium.com/iearn'} target={'_blank'} rel={'noreferrer'}>
+						<span className={'sr-only'}>{'Check our Medium'}</span>
 						<IconMedium className={'w-8 h-8'} />
 					</a>
 				</div>
-				{/* <div className={'px-2 text-black hover:text-black-2 transition-colors cursor-pointer'}>
-					<a href={'https://discord.com/invite/9JBxTWR4nZ'} target={'_blank'} rel={'noreferrer'}>
-						<IconDiscord className={'w-8 h-8'} />
-					</a>
-				</div> */}
 				<div className={'px-2 text-black hover:text-black-2 transition-colors cursor-pointer'}>
 					<a href={'https://gov.yearn.finance/c/projects/keep3r/20'} target={'_blank'} rel={'noreferrer'}>
+						<span className={'sr-only'}>{'Access Keep3r Discourse'}</span>
 						<IconDiscourse className={'w-8 h-8'} />
 					</a>
 				</div>
